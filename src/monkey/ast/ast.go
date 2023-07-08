3be0f9bf549f6703052c57ba671be34a0bd055e6
xela144 <alex.d.bardales@gmail.com>
2023-07-07 19:24:06 -0700

    Return statements

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -48,5 +48,22 @@ func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string {return ls.Token.Literal}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
+type ReturnStatement struct {
+    Token token.Token // token.RETURN
+    ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+    var out bytes.Buffer
+    out.WriteString(rs.TokenLiteral() + " ")
+    if rs.ReturnValue != nil {
+        out.WriteString(rs.ReturnValue.String())
+    }
+    out.WriteString(";")
+
+    return out.String()
+}
 
 
--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -67,6 +67,38 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
     return true
 }
 
+func TestReturnStatements(t *testing.T) {
+    input := `
+return 420;
+return 69;
+`
+    l := lexer.New(input)
+    p := New(l)
+
+    program := p.ParseProgram()
+    checkParserErrors(t, p)
+
+    if len(program.Statements) != 2 {
+        t.Fatalf("program.Statements does not contain 2 statements. got=%d",
+            len(program.Statements))
+    }
+
+    for _, stmt := range program.Statements {
+        returnStmt, ok := stmt.(*ast.ReturnStatement)
+        if !ok {
+            t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
+        }
+
+        if returnStmt.TokenLiteral() != "return" {
+            t.Errorf("returnStmt.TokenLiteral not 'return'. got=%q", returnStmt.TokenLiteral())
+        }
+    }
+
+}
+func testReturnStatement(t *testing.T, s ast.Statement, name string) bool {
+    return true
+}
+
 func checkParserErrors(t *testing.T, p *Parser) {
     errors := p.Errors()
     if len(errors) == 0 {
