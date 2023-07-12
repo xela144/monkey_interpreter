ef2697910ea2575fba70db131ad1b348f3a8d907
xela144 <alex.d.bardales@gmail.com>
2023-07-11 20:51:56 -0700

    Parsing identifiers in expressions

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -60,6 +60,9 @@ func New(l *lexer.Lexer) *Parser {
     p.nextToken()
     p.nextToken()
 
+    p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+    p.registerPrefix(token.IDENT, p.parseIdentifier)
+
     return p
 }
 
@@ -96,7 +99,7 @@ func (p *Parser) parseStatement() ast.Statement {
     case token.RETURN:
         return p.parseReturnStatement()
     default:
-        return nil
+        return p.parseExpressionStatement()
     }
 }
 
@@ -130,6 +133,29 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
     return stmt
 }
 
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+    stmt := &ast.ExpressionStatement{Token: p.curToken}
+    stmt.Expression = p.parseExpression(LOWEST)
+
+    if p.peekTokenIs(token.SEMICOLON) {
+        p.nextToken()
+    }
+    return stmt
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+    prefix := p.prefixParseFns[p.curToken.Type]
+    if prefix == nil {
+        return nil
+    }
+
+    leftExp := prefix()
+    return leftExp
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+    return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
     return p.curToken.Type == t
--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -99,6 +99,36 @@ func testReturnStatement(t *testing.T, s ast.Statement, name string) bool {
     return true
 }
 
+func TestIdentifierExpression(t *testing.T){
+    input := "foobar;"
+
+    l := lexer.New(input)
+    p := New(l)
+    program := p.ParseProgram()
+    checkParserErrors(t, p)
+
+    if len(program.Statements) != 1 {
+        t.Fatalf("program does not have enough statements. got=%d",
+            len(program.Statements))
+    }
+
+    stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+    if !ok {
+        t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+            program.Statements[0])
+    }
+
+    ident, ok := stmt.Expression.(*ast.Identifier)
+    if ident.Value != "foobar" {
+        t.Errorf("ident.Value not %s. got=%s", "foobar", ident.Value)
+    }
+
+    if ident.TokenLiteral() != "foobar" {
+        t.Errorf("ident.TokenLiteral not %s. got=%s", "foobar",
+            ident.TokenLiteral())
+    }
+}
+
 func checkParserErrors(t *testing.T, p *Parser) {
     errors := p.Errors()
     if len(errors) == 0 {
