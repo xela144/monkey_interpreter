fd071ae811c7afe2ef7ef49e2ee116b385eafc27
xela144 <alex.d.bardales@gmail.com>
2023-07-11 20:28:44 -0700

    Add infix and prefix parser types

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -10,9 +10,28 @@ import (
 
 type Parser struct {
     l *lexer.Lexer
+    errors []string
+
     curToken token.Token
     peekToken token.Token
-    errors []string
+
+    prefixParseFns map[token.TokenType]prefixParseFn
+    infixParseFns map[token.TokenType]infixParseFn
+}
+
+
+// Pratt parser: associate parsing functions ("semantic code") with token types.
+type (
+    prefixParseFn func() ast.Expression
+    infixParseFn func(ast.Expression) ast.Expression
+)
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+    p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+    p.infixParseFns[tokenType] = fn
 }
 
 func (p *Parser) nextToken() {
