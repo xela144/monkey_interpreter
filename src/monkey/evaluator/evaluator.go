fe978b6f53bdbb1723c309a62fde63bb09471df1
xela144 <alex.d.bardales@gmail.com>
2023-07-23 16:58:30 -0700

    Convert Boolean objects to native Monkey bools

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -5,6 +5,13 @@ import (
     "monkey/object"
 )
 
+
+var (
+    TRUE = &object.Boolean{Value: true}
+    FALSE = &object.Boolean{Value: false}
+)
+
+
 func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
 
@@ -20,7 +27,7 @@ func Eval(node ast.Node) object.Object {
         return &object.Integer{Value: node.Value}
 
     case *ast.Boolean:
-        return &object.Boolean{Value: node.Value}
+        return nativeBoolToBooleanObject(node.Value)
     }
 
     return nil
@@ -35,4 +42,13 @@ func evalStatements(stmts []ast.Statement) object.Object {
 
 
     return result
+}
+
+
+func nativeBoolToBooleanObject(value bool) *object.Boolean {
+    if value {
+        return TRUE
+    }
+
+    return FALSE
 }
