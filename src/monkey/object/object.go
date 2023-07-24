ef74a3c1c1aa4f88e1e0e67437b031d74dbcf4aa
xela144 <alex.d.bardales@gmail.com>
2023-07-23 19:59:33 -0700

    Evaluating functions (with closures!)

--- src/monkey/object/object.go
+++ src/monkey/object/object.go
@@ -1,6 +1,11 @@
 package object
 
-import "fmt"
+import (
+    "fmt"
+    "monkey/ast"
+    "bytes"
+    "strings"
+)
 
 type ObjectType string
 
@@ -9,6 +14,7 @@ const (
     BOOLEAN_OBJ = "BOOLEAN"
     NULL_OBJ = "NULL"
     RETURN_VALUE_OBJ = "RETURN_VALUE"
+    FUNCTION_OBJ = "FUNCTION"
     ERROR_OBJ = "ERROR"
 )
 
@@ -39,6 +45,30 @@ type ReturnValue struct {
 }
 func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
+
+type Function struct {
+    Parameters []*ast.Identifier
+    Body *ast.BlockStatement
+    Env *Environment
+}
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+    var out bytes.Buffer
+
+    params := []string{}
+    for _, p := range f.Parameters {
+        params = append(params, p.String())
+    }
+
+    out.WriteString("fn")
+    out.WriteString("(")
+    out.WriteString(strings.Join(params, ", "))
+    out.WriteString(") {\n")
+    out.WriteString(f.Body.String())
+    out.WriteString("\n}")
+
+    return out.String()
+}
 
 type Error struct {
     Message string
