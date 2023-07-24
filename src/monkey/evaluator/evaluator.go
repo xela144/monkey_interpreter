d75588f6f0e2c334de8c1035ab0c86b46809396f
xela144 <alex.d.bardales@gmail.com>
2023-07-23 17:18:11 -0700

    Bang operator

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -23,12 +23,17 @@ func Eval(node ast.Node) object.Object {
     case *ast.ExpressionStatement:
         return Eval(node.Expression)
 
-    // Experssions
+    // Expressions
+    case *ast.PrefixExpression:
+        right := Eval(node.Right)
+        return evalPrefixExpression(node.Operator, right)
+
     case *ast.IntegerLiteral:
         return &object.Integer{Value: node.Value}
 
     case *ast.Boolean:
         return nativeBoolToBooleanObject(node.Value)
+
     }
 
     return nil
@@ -44,6 +49,30 @@ func evalStatements(stmts []ast.Statement) object.Object {
 
     return result
 }
+
+func evalPrefixExpression(operator string, right object.Object) object.Object{
+    switch operator {
+    case "!":
+        return evalBangOperatorExpression(right)
+    default:
+        return NULL
+    }
+
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+    switch right {
+    case TRUE:
+        return FALSE
+    case FALSE:
+        return TRUE
+    case NULL:
+        return TRUE
+    default:
+        return FALSE
+    }
+}
+
 
 
 func nativeBoolToBooleanObject(value bool) *object.Boolean {
