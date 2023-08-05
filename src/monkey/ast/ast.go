607827936397a67c57503a4a802f36e6ae752218
xela144 <alex.d.bardales@gmail.com>
2023-08-04 18:52:40 -0700

    Parsing strings

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -251,3 +251,13 @@ func (fl *FunctionLiteral) String() string {
 
     return out.String()
 }
+
+
+type StringLiteral struct {
+    Token token.Token
+    Value string
+}
+func (sl *StringLiteral) expressionNode() {}
+func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
+func (sl *StringLiteral) String() string { return sl.Token.Literal }
+
